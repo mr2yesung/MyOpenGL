7019863047b628279ea7740a747285ed59804d4c
mr2yesung <mryesung@naver.com>
2024-06-06 15:02:39 +0900

    include glew to project

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 /* https://www.glfw.org/documentation.html */
 int main(void)
@@ -19,6 +21,12 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    /* Initialize GLEW entry points after creating valid OpenGL rendering context */
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error GLEW" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
